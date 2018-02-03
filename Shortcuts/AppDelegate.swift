a4bd12b999d94c732f5d2de3904530da93446fb6
Carlos Stockhausen <c@sugarman.tv>
2018-02-03 16:29:29 +0100

    First working state

--- Shortcuts/AppDelegate.swift
+++ Shortcuts/AppDelegate.swift
@@ -7,14 +7,22 @@
 //
 
 import Cocoa
+import Foundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+        }
+        
+        constructMenu()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -22,5 +30,56 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
 
+    @objc func hideDesktopIcons(_ sender: Any?) {
+        
+        @discardableResult
+        func shell(_ args: String...) -> Int32 {
+            let task = Process()
+            task.launchPath = "/usr/bin/defaults"
+            task.arguments = ["write", "com.apple.finder", "CreateDesktop", "false"]
+            task.launch()
+            task.waitUntilExit()
+            return task.terminationStatus
+        }
+        
+        shell("iconshide")
+        
+        restartFinder()
+    }
+    
+    @objc func showDesktopIcons(_ sender: Any?) {
+        
+        @discardableResult
+        func shell(_ args: String...) -> Int32 {
+            let task = Process()
+            task.launchPath = "/usr/bin/defaults"
+            task.arguments = ["write", "com.apple.finder", "CreateDesktop", "true"]
+            task.launch()
+            task.waitUntilExit()
+            return task.terminationStatus
+        }
+        
+        shell("iconsshow")
+        restartFinder()
+    }
+    
+    func restartFinder() {
+        let killTask = Process()
+        killTask.launchPath = "/usr/bin/killall"
+        killTask.arguments = ["Finder"]
+        killTask.launch()
+    }
+    
+    func constructMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Hide Desktop Icons", action: #selector(AppDelegate.hideDesktopIcons(_:)), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Show Desktop Icons", action: #selector(AppDelegate.showDesktopIcons(_:)), keyEquivalent: ""))
+        
+        statusItem.menu = menu
+    }
+
 }
+
+
 
