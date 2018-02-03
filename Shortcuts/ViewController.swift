3df224bdbf4760dcec90bdaf5cd7cc848ab2c535
Carlos Stockhausen <c@sugarman.tv>
2018-02-03 13:25:10 +0100

    Initial Commit

---
+++ Shortcuts/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shortcuts
+//
+//  Created by Carlos Stockhausen on 03.02.18.
+//  Copyright © 2018 Carlos Stockhausen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
